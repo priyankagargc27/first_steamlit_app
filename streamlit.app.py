b2bee1fff0b80b9c0d2878c1c0908f6552c0eda8
Priyanka Garg <40770470+priyankagargc27@users.noreply.github.com>
2023-04-04 15:51:40 -0500

    Update streamlit.app.py
    error

--- streamlit.app.py
+++ streamlit.app.py
@@ -26,7 +26,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 requests.get("https://fruityvice.com/api/fruit/kiwi")
-fruityvice_normalized = pandas.*json_normalize(fruityvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/Kiwi")
 #streamlit.text(fruityvice_response.json())
