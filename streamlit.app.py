ad1bae475de394fc659fd08979b4da92a9f8adc8
Priyanka Garg <40770470+priyankagargc27@users.noreply.github.com>
2023-04-04 10:30:17 -0500

    Update streamlit.app.py
    fix emoji

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,8 +1,8 @@
 import streamlit
 streamlit.title(' My Parents New Healthy diner')
 streamlit.header('🥣 Breakfast Menu')
-streamlit.text(' 🥗Omega 3 and blueberry oatmeal')
-streamlit.text('Kale, spinach and Rocket smoothie')
+streamlit.text(' 🥣Omega 3 and blueberry oatmeal')
+streamlit.text(' 🥗Kale, spinach and Rocket smoothie')
 streamlit.text('🐔Hard Boiled Free-Range egg')
 streamlit.text('🥑🍞 Avacado toast')
 
