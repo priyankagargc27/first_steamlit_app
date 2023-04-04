f5baa350b0cdb5b2e5cb1750bb5d1e909b0a049d
Priyanka Garg <40770470+priyankagargc27@users.noreply.github.com>
2023-04-04 10:40:45 -0500

    Update streamlit.app.py
    Panda changes

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,11 +1,3 @@
-import streamlit
-streamlit.title(' My Parents New Healthy diner')
-streamlit.header('🥣 Breakfast Favroites')
-streamlit.text(' 🥣Omega 3 and blueberry oatmeal')
-streamlit.text(' 🥗Kale, spinach and Rocket smoothie')
-streamlit.text('🐔Hard Boiled Free-Range egg')
-streamlit.text('🥑🍞 Avacado toast')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 Import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
