648764085469e6c9bb523995088cc9a703631201
Priyanka Garg <40770470+priyankagargc27@users.noreply.github.com>
2023-04-04 10:26:42 -0500

    Update streamlit.app.py
    added some text

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,3 +1,9 @@
 import streamlit
 streamlit.title('My Parents New Healthy diner')
 streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and blueberry oatmeal')
+streamlit.text('Kale, spinach and Rocket smoothie')
+streamlit.text('Hard Boiled Free-Range egg')
+
+
+
