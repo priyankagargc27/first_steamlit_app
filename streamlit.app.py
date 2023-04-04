e634a103e6f44fdc750fc8597ea6de2264d7bd48
Priyanka Garg <40770470+priyankagargc27@users.noreply.github.com>
2023-04-04 10:43:08 -0500

    Update streamlit.app.py
    try to fix error

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,6 +1,6 @@
 
-Import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+import pandas
+my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 streamlit.dataframe(my_fruit_list)
 
 
